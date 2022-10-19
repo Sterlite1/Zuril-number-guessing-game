451f2c4204f1036e1c402be461e1bc952e0e4b89
John Gbagbeke <105577276+Sterlite1@users.noreply.github.com>
2022-10-19 21:07:23 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,6 @@
+prompt("Please enter your name");
+let username = ;
+let welMsg = ("You are welcome to stage one of Zuril Number Guessing Game");
+let greeting = username + prompt;
+console.log(greeting);
+ 
